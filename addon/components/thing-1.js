8083c6b8803e23c9d769e44ad33ead901ea040a0
Kyle Turney <turney.kyle@gmail.com>
2017-05-09 13:34:18 -0500

    add ember-cli-stylelint and a component

---
+++ addon/components/thing-1.js
@@ -0,0 +1,6 @@
+import Ember from 'ember';
+import layout from '../templates/components/thing-1';
+
+export default Ember.Component.extend({
+  layout
+});
---
+++ app/components/thing-1.js
@@ -0,0 +1,1 @@
+export { default } from 'inline-precompile-error/components/thing-1';
